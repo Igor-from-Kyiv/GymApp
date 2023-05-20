b97951ccbeb647591d38c45ba7c45e2a67dadd51
Igor <igorkramarenko86@gmail.com>
2023-05-20 22:58:56 +0300

    implementation of workouts log

---
+++ app/src/main/java/com/example/gymapp/WorkoutsListActivity.kt
@@ -0,0 +1,27 @@
+package com.example.gymapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.RecyclerView
+import com.example.gymapp.databinding.ActivityWorkoutsListBinding
+import com.example.gymapp.recyclerViewAdapter.ItemAdapter
+
+class WorkoutsListActivity : AppCompatActivity() {
+    lateinit var binding: ActivityWorkoutsListBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityWorkoutsListBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Initialize data.
+        val workoutsDataset = AppDataBase(this).getWorkoutsList()
+        println(workoutsDataset.toString())
+
+        val recyclerView = findViewById<RecyclerView>(R.id.workouts_list)
+        recyclerView.adapter = ItemAdapter(this, workoutsDataset)
+
+        // Use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        recyclerView.setHasFixedSize(true)
+    }
+}
